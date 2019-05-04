f53c02ea572e47aba137714fdc2be9797494a34e
aacpt-iscteiul <aacpt@iscte-iul.pt>
2019-05-04 10:47:51 +0100

    commit do projeto maven

---
+++ ProjetoDeTeste/src/main/java/GrupoDeTrabalho/ProjetoDeTeste/App.java
@@ -0,0 +1,13 @@
+package GrupoDeTrabalho.ProjetoDeTeste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
