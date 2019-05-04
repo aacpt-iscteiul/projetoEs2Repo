e201ffb75232b680fe5cf1bf43efeee1024591b9
aacpt-iscteiul <aacpt@iscte-iul.pt>
2019-05-04 11:18:41 +0100

    second commit

--- ProjetoDeTeste/src/main/java/GrupoDeTrabalho/ProjetoDeTeste/App.java
+++ ProjetoDeTeste/src/main/java/GrupoDeTrabalho/ProjetoDeTeste/App.java
@@ -4,10 +4,9 @@ package GrupoDeTrabalho.ProjetoDeTeste;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App{
+	
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
 }
